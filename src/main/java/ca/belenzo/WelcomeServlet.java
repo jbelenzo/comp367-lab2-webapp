398276cd8b1a6d3c51e7b4c528be0cfd16c0be0d
jbelenzo <jbelenzo@my.centennialcollege.ca>
2025-03-15 22:06:23 -0400

    Modified WelcomeServlet.java to display Good Morning or Good Afternoon based on the time of day
    Fix #1

--- src/main/java/ca/belenzo/WelcomeServlet.java
+++ src/main/java/ca/belenzo/WelcomeServlet.java
@@ -3,16 +3,28 @@ package ca.belenzo;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import java.time.LocalTime;
 
 public class WelcomeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Set response content type
         response.setContentType("text/html");
 
-        // Print "Welcome to COMP367"
+        // Get the current time
+        LocalTime currentTime = LocalTime.now();
+
+        // Determine whether it's morning or afternoon
+        String greeting;
+        if (currentTime.isBefore(LocalTime.NOON)) {
+            greeting = "Good morning, Jasper, Welcome to COMP367";
+        } else {
+            greeting = "Good afternoon, Jasper, Welcome to COMP367";
+        }
+
+        // Print the greeting
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>Welcome to COMP367</h1>");
+        out.println("<h1>" + greeting + "</h1>");
         out.println("</body></html>");
     }
 }
